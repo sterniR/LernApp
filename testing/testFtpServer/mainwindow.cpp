5a2fcf1e6c3d521de330fa82cfc2323b84dcb601
paryskro <roman.paryskura@protonmail.com>
2025-02-07 05:11:10 +0100

    ftpserver test done

--- testing/testFtpServer/mainwindow.cpp
+++ testing/testFtpServer/mainwindow.cpp
@@ -7,16 +7,16 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     dataChannel = new FtpDataChannel(this);
+    controlChannel = new FtpControlChannel(this);
 
     connect(dataChannel, &FtpDataChannel::dataReceived, this, &MainWindow::dataReceived);
     connect(controlChannel, &FtpControlChannel::reply, this, &MainWindow::serverReply);
     connect(controlChannel, &FtpControlChannel::opened, this, &MainWindow::serverConnected);
 
-    controlChannel = new FtpControlChannel(this);
 
-    ftpAddress = "127.0.0.1/";
-    username = "myuser";
-    password = "123456?!§$%";
+    ftpAddress = "127.0.0.1";
+    username = "test";
+    password = "123456";
 
     controlChannel->connectToServer(ftpAddress);
 }
@@ -75,4 +75,45 @@ void MainWindow::serverConnected(const QHostAddress &address, int port) {
     getFileList();
 }
 
-void MainWindow::serverReply(int code,)
+void MainWindow::serverReply(int code, const QString &parameters) {
+    if(code == 150 && uploadFileName != "") {
+        QFile *file = new QFile(ui->uploadFileInput->text());
+        QFileInfo fileInfo(*file);
+        uploadFileName = fileInfo.fileName();
+        if (file->open(QIODevice::ReadOnly)) {
+            QThread::msleep(1000);
+            QByteArray data = file->readAll();
+            dataChannel->sendData(data + "\n\r");
+            qDebug() << data;
+        } else {
+            QMessageBox::warning(this, "Invalid File", "Failed to open file for upload.");
+        }
+    }
+    if(code == 226 && uploadFileName != "") {
+        uploadFileName = "";
+        QMessageBox::warning(this, "Upload Success", "File successfully uploaded.");
+    }
+}
+
+void MainWindow::dataReceived(const QByteArray &data) {
+    if(data.startsWith("type=file")) {
+        ui->listWidget->clear();
+        QStringList fileList = QString(data).split("\r\n");
+        if (fileList.length() > 0) {
+            for (int i = 0; i < fileList.length(); ++i) {
+                if (fileList.at(i) != "") {
+                    QStringList fileInfo = fileList.at(i).split(";");
+                    QString fileName = fileInfo.at(4).simplified();
+                    ui->listWidget->addItem(fileName);
+                }
+            }
+        } else {
+            QString folder = ui->downloadPath->text();
+            QFile file(folder + "/" + downloadFileName);
+            file.open(QIODevice::WriteOnly);
+            file.write((data));
+            file.close();
+            QMessageBox::information(this, "Success", "File successfully downloaded.");
+        }
+    }
+}
